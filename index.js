0f117afaaecec2deb884d2dedbc9df3f4818245e
vishalicious213 <vishalicious213@gmail.com>
2023-04-18 17:52:05 -0700

    added event listeners to history & team nav items

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const main = document.getElementById("main")
+const menu01 = document.getElementById("menu-01")
+const menu02 = document.getElementById("menu-02")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+menu01.addEventListener("click", function() {
+    console.log("history")
+})
+
+menu02.addEventListener("click", function() {
+    console.log("team")
+})
+
+// ⬇️ RENDER THE APP ⬇️
 
 function renderMain() {
     main.style.backgroundImage = `url(/img/background-1.jpg)`
